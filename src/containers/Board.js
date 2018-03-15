656c9331934280a957182eae1f90a2161cabcc19
Dave Ellis <dave@ellisnetmail.co.uk>
2018-03-15 15:12:08 +0000

    added logic for checking if a game is won, currently logs boolean result of check to console

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -50,6 +50,48 @@ class Board extends Component{
         requiredSquare.colour = this.state.activeColour;
         const newActiveColour = this.swapActiveColour();
         this.setState({rows: allRows, activeColour: newActiveColour});
+        console.log("Game Result", this.checkIfWinnerExists());
+    }
+
+    checkIfWinnerExists(){
+
+        const rowOne = this.state.rows[0];
+        const rowTwo = this.state.rows[1];
+        const rowThree = this.state.rows[2];
+        
+        const columnOne     = [this.state.rows[0][0], this.state.rows[1][0], this.state.rows[2][0]];
+        const columnTwo     = [this.state.rows[0][1], this.state.rows[1][1], this.state.rows[2][1]];
+        const columnThree   = [this.state.rows[0][2], this.state.rows[1][2], this.state.rows[2][2]];
+
+        const topLeftDiagonal       = [this.state.rows[0][0], this.state.rows[1][1], this.state.rows[2][2]];
+        const bottomLeftDiagonal    = [this.state.rows[2][0], this.state.rows[1][1], this.state.rows[0][2]];
+
+        const squaresToCheck = [rowOne, rowTwo, rowThree, columnOne, 
+                                columnTwo, columnThree, topLeftDiagonal, bottomLeftDiagonal];
+        
+        let winnerExists = false;                                
+        squaresToCheck.forEach(squares => {
+            if(this.checkSquareColoursMatch(squares)){
+                winnerExists = true;
+                return;
+            }
+        })
+
+        return winnerExists;
+    }
+
+    checkSquareColoursMatch(squares){
+        const squareOne = squares[0];
+        const squareTwo = squares[1];
+        const squareThree = squares[2];
+
+        if (squareOne.colour === "white-square" || squareTwo.colour === "white-square" || squareThree.colour === "white-square") return false;
+
+        if(squareOne.colour === squareTwo.colour && squareTwo.colour === squareThree.colour){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     render(){
